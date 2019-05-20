062319cc7b36c525e7410efac3420babc6b98104
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-05-20 20:34:24 +0530

    Prepared for deployment on Heroku

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI:
-    'mongodb+srv://niweera:%23Srilanka96@niweeratvdb-kj4ap.mongodb.net/niweeratvdb',
-    secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
